e6bb426c71f7f77149bd340d0beefddca52ff673
rwciANA <145408428+rwciANA@users.noreply.github.com>
2025-05-06 17:30:37 +0200

    Update multiplicaion

--- calculator.java
+++ calculator.java
@@ -5,9 +5,17 @@ public class calculator{
    }
 public int add (int a , int b ){
     int suma = a +b ;
-      int zz= 2133;
+      
    return suma ;
 }
+public int res(int a, int b){
+
+   return a-b;
+}public mul(int a. int b){
+
+   return a*b;
+}
+
 
 
 }
